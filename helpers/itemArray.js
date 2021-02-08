2f3c7199d8ea9c2f932a8e6c3269fdb21e1c4059
Danish Raza <rdanishraza@gmail.com>
2021-02-08 11:08:46 +0530

    first commit

---
+++ helpers/itemArray.js
@@ -0,0 +1,34 @@
+module.exports = [
+	{
+		name: "Chips",
+		category: "Food and Beverages",
+		available_at: [
+			{ 
+				address: "24x7 Sector 54, Gurgaon",
+				lat: 12.21,
+				long: 28.72
+			},
+			{ 
+				address: "Big Bazaar, Sector 25, Gurgaon",
+				lat: 12.23,
+				long: 28.79
+			}
+		]
+	},
+	{
+		name: "Disprin",
+		category: "Pharmacy",
+		available_at: [
+			{ 
+				address: "Apollo Medicine, Sector 63, Gurgaon",
+				lat: 12.25,
+				long: 28.52
+			},
+			{ 
+				address: "Apollo Medicine, Sector 22, Gurgaon",
+				lat: 12.20,
+				long: 28.29
+			}
+		]
+	},
+]
